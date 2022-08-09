041c6e3edc68be19121077a8a06024c3759d677a
MariT29fsh <161764@unsaac.edu.pe>
2022-08-09 00:37:58 -0500

    first

---
+++ src/Pantallas/MyNotes/MyNotes.js
@@ -0,0 +1,40 @@
+import MainScreen from '../../components/MainScreen'; 
+import {Link} from 'react-router-dom';
+import {Button, Card} from 'react-bootstrap';
+const MyNotes =() =>{
+return(
+    <div>
+    <MainScreen title='Bienvenido '>
+        <Link to='/mynotes/create'>
+            <Button style={{marginLeft:10, marginBottom:6}} size="lg">
+                Que va hacer hoy dia?
+            </Button>
+            <Card>
+            <Card.Header style={{display:'flex'}}>
+            <span
+                    // onClick={() => ModelShow(note)}
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
+                 />
+                <div>
+                    <Button>View  </Button>
+                </div>
+            </Card.Header>
+            </Card>
+            
+
+
+        </Link>
+
+    </MainScreen>
+    
+    </div>
+)
+}
+export default MyNotes;
