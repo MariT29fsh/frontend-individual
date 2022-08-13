7912159e5f12d00a4cab9446aba8774092a6bad4
MariT29fsh <161764@unsaac.edu.pe>
2022-08-13 02:08:28 -0500

    updating1 cors

--- src/Pantallas/CreateNotes/Create.js
+++ src/Pantallas/CreateNotes/Create.js
@@ -34,7 +34,7 @@ const Create = () => {
         console.log(title,description,image);
         try {
             const {data} = await axios.post('https://backend-photos.herokuapp.com/api/mynotes/create', 
-            {title, description, image}, config
+            {title, description, image}
         
             )
             if  (data.success === true){
@@ -51,7 +51,6 @@ const Create = () => {
 
     }
 
-  
 
   return (
    <>
